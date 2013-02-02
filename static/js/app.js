d7be4da14c2c3fed2d7e7cf4d19ae0a261e78429
maxjchung <max@maxjchung.com>
2013-02-01 18:10:13 -0800

    Tweak tagline, first card positions.

--- static/js/app.js
+++ static/js/app.js
@@ -35,7 +35,7 @@ var CardsyApp = {
 
     var introText = 'Make new cards by clicking the canvas.';
 
-    setTimeout(function () { CardsyApp.addCard(50, 50, introText) }, 750);
+    setTimeout(function () { CardsyApp.addCard(20, 60, introText) }, 710);
 
   },
 
